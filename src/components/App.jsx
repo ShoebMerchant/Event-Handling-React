160b924fed0269c2356b5da95876d1ece6986c1e
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-25 16:30:05 +0530

    passing object in useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,41 +2,71 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
+
   const [mouseOver, setMouseOver] = useState(false);
-  const [name, setName] = useState("");
-  const [displayName, setDisplayName] = useState("");
+
+  const [displayFName, setDisplayFName] = useState("");
+
+  const [name, setName] = useState({
+    fName: "",
+    lName: "",
+  });
 
   function handleClick(event) {
     setHeadingText("Submitted");
-    setDisplayName(name);
+    setDisplayFName(name.fName);
     event.preventDefault();
+  }
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    // console.log(name, value);
+    /**
+     * Do not use event inside setName() because
+     * it's a synthetic event
+     */
+    setName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+        };
+      }
+    });
   }
 
   function handleMouseOver() {
     setMouseOver(true);
   }
-
   function handleMouseOut() {
     setMouseOver(false);
-  }
-
-  function handleChange(event) {
-    const { value: newName } = event.target;
-    setName(newName);
   }
 
   return (
     <div className="container">
       <h1>
         {headingText} {headingText === "Hello" ? " " : " by "}
-        {displayName}
+        {name.fName} {name.lName}
       </h1>
       <form onSubmit={handleClick}>
         <input
+          name="fName"
           type="text"
           onChange={handleChange}
-          placeholder="What's your name?"
-          value={name}
+          placeholder="What's your first name?"
+          value={name.fName}
+        />
+        <input
+          name="lName"
+          type="text"
+          onChange={handleChange}
+          placeholder="What's your last name?"
+          value={name.lName}
         />
         <button
           style={{ backgroundColor: mouseOver ? "black" : "white" }}
